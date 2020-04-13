35def698dd08359888b8adf00f0ea07b881b5b48
Mykhailo Lohvin <mihail1995dark@gmail.com>
2020-04-13 20:50:27 +0300

    task done

---
+++ testForAnton/testForAnton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testForAnton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            var number = Console.ReadLine();
+            Console.WriteLine($"you enter {number}");
+            Console.ReadLine();
+        }
+    }
+}
